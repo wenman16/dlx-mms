baeb4f0ac4db356c2783a25a75b8fa70b84fe0db
Wen Man <wendylmancia@gmail.com>
2022-04-29 13:12:14 +0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,6 +45,7 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::prefix('api')
                 ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
         });
     }
